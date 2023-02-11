2314d4cd020414ee87475985bcc84f346df10d20
khuzi-shaikh <124673301+khuzi-shaikh@users.noreply.github.com>
2023-02-11 12:06:50 +0530

    Updated

--- src/Component/BulbOnOff.jsx
+++ src/Component/BulbOnOff.jsx
@@ -1,20 +1,20 @@
 import { Button } from "@mui/material";
 import React, { useState } from "react";
 
-
 export const BulbOnOff = () => {
   const [toggle, setToggle] = useState(false);
   const toggleButton = () => {
-    setInterval(()=>{
-      setToggle(!toggle)
-    },2000)
+    // setInterval(() => {
+      setToggle(!toggle);
+    // }, 1000);
   };
-
-  console.log(toggle)
-
   return (
-    <div className="div"><br /><br /><br /><br />
-      <img src={ `./image/bulb_${toggle ?"on":"off"}.png`} />
+    <div className="div">
+      <br />
+      <br />
+      <br />
+      <br />
+      <img src={`./image/bulb_${toggle ? "on" : "off"}.png`} />
       <Button
         onClick={toggleButton}
         style={{
--- src/Component/CounterTask.jsx
+++ src/Component/CounterTask.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 export const CounterTask = () => {
   const [count, setCount] = useState(0);
   const handleInc = () => {
-    count>=10 ? setCount(count*2) : setCount(count+1) 
+    count >= 10 ? setCount(count * 2) : setCount(count + 1);
     // if (count <= 9) {
     //   setCount(count + 1);
     // } else if (count <= 19) {
@@ -14,7 +14,7 @@ export const CounterTask = () => {
     // }
   };
   const handleDec = () => {
-    count >0 && setCount(count-1)
+    count > 0 && setCount(count - 1);
     // if (count > 0) {
     //   setCount(count - 1);
     // } else {
@@ -24,23 +24,31 @@ export const CounterTask = () => {
   return (
     <div>
       <div className="a">
-        <h1 className="h">This Is Counter 🔄</h1><br />
-        <h2 style={{color:count>0 ? "green":"red" }}>
-          {count}
-        </h2><br /><br />
+        <h1 className="h">This Is Counter 🔄</h1>
+        <br />
+        <h2 style={{ color: count > 0 ? "green" : "red" }}>{count}</h2>
+        <br />
+        <br />
         <Button variant="contained" color="success" onClick={handleInc}>
           +
-        </Button><br /><br />{" "} 
-        <span> </span>
-        <Button variant="contained" color="error" onClick={handleDec}>
+        </Button>
+        <br />
+        <br /> <span> </span>
+        <Button
+          variant="contained"
+          disabled={count == 0}
+          color="error"
+          onClick={handleDec}
+        >
           -
-        </Button><br /><br />{" "}
-        <span> </span>
+        </Button>
+        <br />
+        <br /> <span> </span>
         <Button
           variant="outlined"
           count
           onClick={() => setCount(0)}
-          disabled={count === 0}
+          disabled={count == 0}
         >
           Reset
         </Button>
